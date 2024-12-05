6e5a990126cef0b1af2bb95d74b8f256d3ab6e05
Abhisht Jindal <64840557+Abhisht23@users.noreply.github.com>
2024-12-06 01:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,7 @@ window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
-	<head>
-			<!-- Google tag (gtag.js) -->
-			<script async src="https://www.googletagmanager.com/gtag/js?id=G-E912MLRESC"></script>
-			<script>
-			  window.dataLayer = window.dataLayer || [];
-			  function gtag(){dataLayer.push(arguments);}
-			  gtag('js', new Date());
-			
-			  gtag('config', 'G-E912MLRESC');
-			</script>
-	</head>
+	
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
